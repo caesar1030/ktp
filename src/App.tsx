d0c1361f3343859c9d9f1f3365a7c36e9ef545f5
zlx454545 <zlx454545@gmail.com>
2024-04-21 16:32:38 +0900

    reafactor: slider 컴포넌트 compound 패턴 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+// import Slider from './common-ui/slider';
+
 import Slider from './common-ui/slider';
 
 const App = () => {
   return (
     <Slider>
-      <div>Slide 1</div>
-      <div>Slide 2</div>
-      <div>Slide 3</div>
+      <Slider.SlidesContainer>
+        <Slider.Slide>
+          <div>slide 1</div>
+        </Slider.Slide>
+        <Slider.Slide>
+          <div>slide 2</div>
+        </Slider.Slide>
+        <Slider.Slide>
+          <div>slide 3</div>
+        </Slider.Slide>
+      </Slider.SlidesContainer>
+
+      <Slider.LeftButton>{'<'}</Slider.LeftButton>
+      <Slider.RightButton>{'>'}</Slider.RightButton>
     </Slider>
   );
 };
--- src/common-ui/slider/index.tsx
+++ src/common-ui/slider/index.tsx
@@ -1,10 +1,15 @@
-import { Children, ReactNode, useRef, useState } from 'react';
+import {
+  Children,
+  MutableRefObject,
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import * as S from './styles';
 import useInterval from '../../hooks/use-interval';
-
-interface SliderProps {
-  children: ReactNode[];
-}
 
 const AUTO_SLIDE_TERM = 5000;
 const Directions = {
@@ -12,34 +17,61 @@ const Directions = {
   RIGHT: 1,
 } as const;
 
+interface SlideContextType {
+  move: (direction: keyof typeof Directions) => void;
+  slidesContainerRef: MutableRefObject<HTMLDivElement>;
+  totalLengthRef: MutableRefObject<number>;
+  slideIndex: number;
+}
+
+const SliderContext = createContext<SlideContextType>(null!);
+
+interface SliderProps {
+  children: ReactNode;
+}
+
+interface SlidesContainerProps {
+  children: ReactNode[];
+}
+
+interface SlideProps {
+  children: ReactNode;
+}
+
+interface LeftButtonProps {
+  children: ReactNode;
+}
+
+interface RightButtonProps {
+  children: ReactNode;
+}
+
 const Slider = ({ children }: SliderProps) => {
   const [slideIndex, setSlideIndex] = useState(1);
-  const slidesWrapperRef = useRef<HTMLDivElement>(null!);
-
-  const slides = [
-    Children.toArray(children)[children.length - 1],
-    ...Children.toArray(children),
-    Children.toArray(children)[0],
-  ];
-
-  const fakeLength = slides.length;
+  const slidesContainerRef = useRef<HTMLDivElement>(null!);
+  const totalLengthRef = useRef<number>(0);
 
   const replaceSlideWithoutAnimation = (toReplaceIndex: number) => {
-    slidesWrapperRef.current.style.transition = 'none';
-    slidesWrapperRef.current.style.transform = `translateX(-${
+    slidesContainerRef.current.style.transition = 'none';
+    slidesContainerRef.current.style.transform = `translateX(-${
       toReplaceIndex * 100
     }%)`;
 
     requestAnimationFrame(() => {
-      slidesWrapperRef.current.style.transition = '';
-      slidesWrapperRef.current.style.transform = '';
+      slidesContainerRef.current.style.transition = '';
+      slidesContainerRef.current.style.transform = '';
     });
   };
 
   const move = (direction: keyof typeof Directions) => {
+    if (!totalLengthRef.current) return;
+
     const hasToReplace =
-      direction === 'LEFT' ? slideIndex <= 1 : slideIndex >= fakeLength - 2;
-    const toReplaceIndex = direction === 'LEFT' ? fakeLength - 1 : 0;
+      direction === 'LEFT'
+        ? slideIndex <= 1
+        : slideIndex >= totalLengthRef.current - 2;
+    const toReplaceIndex =
+      direction === 'LEFT' ? totalLengthRef.current - 1 : 0;
 
     if (hasToReplace) {
       replaceSlideWithoutAnimation(toReplaceIndex);
@@ -54,17 +86,69 @@ const Slider = ({ children }: SliderProps) => {
   useInterval(() => move('RIGHT'), AUTO_SLIDE_TERM);
 
   return (
-    <S.Slider>
-      <S.SlidesContainer ref={slidesWrapperRef} $slideindex={slideIndex}>
-        {slides.map((slide, idx) => (
-          <S.Slide key={idx}>{slide}</S.Slide>
-        ))}
-      </S.SlidesContainer>
-
-      <S.LeftButton onClick={() => move('LEFT')}> {'<'}</S.LeftButton>
-      <S.RightButton onClick={() => move('RIGHT')}> {'>'}</S.RightButton>
-    </S.Slider>
+    <SliderContext.Provider
+      value={{
+        move,
+        slidesContainerRef,
+        totalLengthRef,
+        slideIndex,
+      }}
+    >
+      <S.Slider>{children}</S.Slider>
+    </SliderContext.Provider>
   );
 };
+
+const SlidesContainer = ({ children }: SlidesContainerProps) => {
+  const context = useContext(SliderContext);
+  if (!context)
+    throw new Error('SlidesContainer가 SliderContext 외부에서 사용');
+
+  const { slidesContainerRef, totalLengthRef, slideIndex } = context;
+  const slides = [
+    Children.toArray(children)[children.length - 1],
+    ...Children.toArray(children),
+    Children.toArray(children)[0],
+  ];
+
+  useEffect(() => {
+    totalLengthRef.current = slides.length;
+  }, [slides.length, totalLengthRef]);
+
+  return (
+    <S.SlidesContainer ref={slidesContainerRef} $slideindex={slideIndex}>
+      {slides}
+    </S.SlidesContainer>
+  );
+};
+
+const Slide = ({ children }: SlideProps) => {
+  return <S.Slide>{children}</S.Slide>;
+};
+
+const LeftButton = ({ children }: LeftButtonProps) => {
+  const context = useContext(SliderContext);
+  if (!context) throw new Error('LeftButton이 SliderContext 외부에서 사용');
+
+  const { move } = context;
+
+  return <S.LeftButton onClick={() => move('LEFT')}>{children}</S.LeftButton>;
+};
+
+const RightButton = ({ children }: RightButtonProps) => {
+  const context = useContext(SliderContext);
+  if (!context) throw new Error('RightButton이 SliderContext 외부에서 사용');
+
+  const { move } = context;
+
+  return (
+    <S.RightButton onClick={() => move('RIGHT')}>{children}</S.RightButton>
+  );
+};
+
+Slider.SlidesContainer = SlidesContainer;
+Slider.Slide = Slide;
+Slider.LeftButton = LeftButton;
+Slider.RightButton = RightButton;
 
 export default Slider;
